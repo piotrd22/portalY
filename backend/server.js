869236d99cc8c4e85c2402516aef864d49844b00
Piotr Damrych <piotr.damrych2002@gmail.com>
2024-01-09 23:12:51 +0100

    new cors config

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const server = https.createServer(
 
 // CORS configuration
 const corsOptions = {
-  origin: "*",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   optionSuccessStatus: 200,
 };
